75f6736d7986a4e65fcb8e7da94ec12b7146b91e
Arthur Kung <arthurkung1006@gmail.com>
2020-11-11 00:27:00 +0000

    ask for integer and operation input

--- BasicCalculator.cpp
+++ BasicCalculator.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
-
+int askForInt()
+{
+    std::cout << "please give me an integer\n";
+    int x;
+    std::cin >> x;
+    return x;
+}
+char askForOperation()
+{
+    char x;
+    while (true)
+    {
+        std::cout << "please give me an an operation ( + , - , * or / )\n";
+        std::cin >> x;
+        switch (x)
+        {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            return x;
+        default:
+            std::cout << "undefined operation, please check!\n";
+        }
+    }
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    int x;
+    x = askForInt();
+    
+    char y;
+    y = askForOperation();
+
+
+    std::cout << "Hello! You have inserted: " << y;
+    return 0;
 }
