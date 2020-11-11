8d7b283ff964bbc5955328ce0762644546b4df8d
Arthur Kung <arthurkung1006@gmail.com>
2020-11-11 00:27:28 +0000

    functions for +-*/

--- BasicCalculator.cpp
+++ BasicCalculator.cpp
@@ -25,6 +25,22 @@ char askForOperation()
         }
     }
 }
+int add(int x, int y)
+{
+    return x + y;
+}
+int subtract(int x, int y)
+{
+    return x - y;
+}
+int multiply(int x, int y)
+{
+    return x * y;
+}
+int divide(int x, int y)
+{
+    return x / y;
+}
 int main()
 {
     int x;
